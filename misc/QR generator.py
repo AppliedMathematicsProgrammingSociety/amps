cf37c6a1abfe682bc32356362d87ce2634f7bec3
Rafiqunnabi-Zihad <117552169+Rafiqunnabi-Zihad@users.noreply.github.com>
2023-02-04 21:20:29 +0600

    Update QR generator.py

--- misc/QR generator.py
+++ misc/QR generator.py
@@ -3,3 +3,5 @@ import qrcode as qr
 text = f'https://drive.google.com/file/d/13O7fdPmiro1Z2wRYAaPA3X8a_RqN_N63/view?usp=sharing'
 img = qr.make(text)
 img.save(f'AMPS_doctrine.svg')
+
+#https://pypi.org/project/qrcode/
