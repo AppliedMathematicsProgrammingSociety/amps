9e8f19f8a186e2b0835d423aafbdfd68ef1909db
evanmozumder <66359246+evanmozumder@users.noreply.github.com>
2023-03-10 22:15:40 +0600

    conio.h library for linux

---
+++ misc/TicTacToe (console_based)/conio.h
@@ -0,0 +1,6 @@
+// this file is only for linux user
+// because conio.h is not availabe in linux
+// cd /usr/include
+// gedit conio.h
+// https://github.com/evanmozumder/conio.h/blob/master/conio.h
+// save it (done)
