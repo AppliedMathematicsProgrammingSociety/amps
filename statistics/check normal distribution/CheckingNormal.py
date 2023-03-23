6006668d0de70f8194065211d1d7f727a4e65f01
Muhammad-Sazzad-Abrar <124142616+Muhammad-Sazzad-Abrar@users.noreply.github.com>
2023-03-23 20:16:52 +0600

    Update CheckingNormal.py

--- statistics/check normal distribution/CheckingNormal.py
+++ statistics/check normal distribution/CheckingNormal.py
@@ -1,6 +1,6 @@
 # Using Histogram
 
-print("\n Checking by using Histogram\n\n")
+print("\n Checking by using Histogram-\n")
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,11 +13,11 @@ bins = range(min(data), max(data) + bin_width, bin_width)
 plt.hist(data, bins=bins)
 plt.show()
 
-print("From the histogram we can say that the ",data," does not follows the Normal distribution.\n\n")
+print(data, " does not follows the Normal distribution.\n")
 
-# Varifying using empirical rule (68-95-99.7)
+# Verifying using empirical rule (68-95-99.7)
 
-print("Varifying using empirical rule\n\n")
+print("Verifying using empirical rule-\n")
 
 mean = np.mean(data)
 std = np.std(data)
@@ -26,18 +26,18 @@ print(F"Mean: {mean}")
 print(F"Standard deviation: {std}\n")
 
 oneStd = (mean - std, mean + std)
-twoStd = (mean - 2*std, mean + 2*std)
-threeStd = (mean - 3*std, mean + 3*std)
+twoStd = (mean - 2 * std, mean + 2 * std)
+threeStd = (mean - 3 * std, mean + 3 * std)
 
-within_oneStd = len([x for x in data if x >= oneStd[0] and x <= oneStd[1]])
-within_twoStd = len([x for x in data if x >= twoStd[0] and x <= twoStd[1]])
-within_threeStd = len([x for x in data if x >= threeStd[0] and x <= threeStd[1]])
+within_oneStd = len([x for x in data if oneStd[0] <= x <= oneStd[1]]) / len(data)
+within_twoStd = len([x for x in data if twoStd[0] <= x <= twoStd[1]]) / len(data)
+within_threeStd = len([x for x in data if threeStd[0] <= x <= threeStd[1]]) / len(data)
 
-print(f"Percentage of data within one standard deviation of the mean: {within_oneStd / len(data) * 100}%")
-print(f"Percentage of data within two standard deviation of the mean: {within_twoStd / len(data) * 100}%")
-print(f"Percentage of data within three standard deviation of the mean: {within_threeStd / len(data) * 100}%")
+print(f"Percentage of data within one standard deviation of the mean: {within_oneStd * 100}%")
+print(f"Percentage of data within two standard deviation of the mean: {within_twoStd * 100}%")
+print(f"Percentage of data within three standard deviation of the mean: {within_threeStd * 100}%\n")
 
-if within_oneStd >= 0.68 and within_twoStd >= 0.95 and within_threeStd >= 0.997:
-    print(data, "follows the Normal distribution")
+if within_oneStd < 0.68 or within_twoStd < 0.95 or within_threeStd < 0.997:
+    print(data, "does not follows the Normal distribution.")
 else:
-    print(data, "does not follows the Normal distribution")
+    print(data, "follows the Normal distribution")
